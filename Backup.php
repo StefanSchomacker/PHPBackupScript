3e693ac6ff3a445a41fad009fec8a1e97e72d0b9
Stefan Schomacker <StefanSchomacker@users.noreply.github.com>
2016-08-31 18:04:49 +0200

    getter and setter added + log file created

--- Backup.php
+++ Backup.php
@@ -1,8 +1,11 @@
 <?php
 
 $backupDir = $_SERVER['DOCUMENT_ROOT'] . "/backup/";
-$backup = new backup($backupDir, "backup@example.com");
-$backup->startBackup();
+
+$backup = new backup($backupDir);
+$backup->setMail("backup@example.com");
+
+$backup->execute();
 
 class backup
 {
@@ -10,29 +13,57 @@ class backup
     private $_recDirIt;
     private $_recItIt;
 
-    private $_email;
+    //zip values
+    private $_zipNumFiles = null;
+    private $_zipStatus = null;
+
+    //default values
+    private $_mail = null;
+    private $_createLog = true;
 
 
-    public function __construct($backupDir, $email = null)
+    public function __construct($backupDir)
     {
         $this->_backupDir = $backupDir;
         $this->_recDirIt = new RecursiveDirectoryIterator($_SERVER['DOCUMENT_ROOT'], RecursiveDirectoryIterator::SKIP_DOTS);
         $this->_recItIt = new RecursiveIteratorIterator($this->_recDirIt);
-
-        $this->_email = $email;
     }
 
-    public function startBackup()
+    public function execute()
     {
+        $success = true;
+
+        //10 minutes
+        ini_set('max_execution_time', 600);
+
+        if ($this->_createLog) {
+            $this->createNewLogEntry("start");
+        }
+
         $zipPath = $this->backup($this->_recItIt);
 
         if (is_null($zipPath)) {
-            return false;
+            $success = false;
+
+            if ($this->_createLog) {
+                $this->createNewLogEntry("error", "Zip Archive cannot be created");
+            }
         }
-        if (!is_null($this->_email)) {
-            return $this->sendMail($zipPath);
+
+        if (!is_null($this->_mail)) {
+            $success = $this->sendMail($zipPath);
+
+            if (!$success && $this->_createLog) {
+                $this->createNewLogEntry("error", "E-Mail cannot be sent");
+            }
         }
-        return true;
+
+        if ($this->_createLog) {
+            $this->createNewLogEntry("end");
+        }
+
+
+        return $success;
     }
 
     private function backup($recItIt)
@@ -45,7 +76,9 @@ class backup
 
         foreach ($recItIt as $singleItem) {
 
-            if (dirname($singleItem) . "/" == $this->_backupDir) {
+
+            //exclude backup folder
+            if (is_int(strpos($singleItem, $this->_backupDir))) {
                 continue;
             }
 
@@ -63,8 +96,12 @@ class backup
         }
 
         if ($zip->close()) {
+
+            $this->_zipNumFiles = $zip->numFiles;
+            $this->_zipStatus = $zip->status;
             return $zipPath;
         }
+
         return null;
     }
 
@@ -91,7 +128,95 @@ class backup
         $message .= $content . "\r\n\r\n";
         $message .= "--" . $part . "--";
 
-        return mail($this->_email, $subject, $message, $header);
+        return mail($this->_mail, $subject, $message, $header);
+    }
+
+    private function createNewLogEntry($type, $message = null)
+    {
+
+        $currentYear = date("Y");
+        $currentMonth = date("F");
+
+
+        //log folder
+        $path = $this->_backupDir . "log/";
+        if (!file_exists($path)) {
+            mkdir($path, 0777, true);
+        }
+
+        //year folder
+        $path = $path . $currentYear . "/";
+        if (!file_exists($path)) {
+            mkdir($path, 0777, true);
+        }
+
+        //log file
+        $path = $path . "backup-" . strtolower($currentMonth) . ".log";
+
+        $logType = "UNDEFINED";
+        $logMessage = "...";
+
+        switch ($type) {
+            case "start":
+                $logType = "NOTICE";
+                $logMessage = "Backup started";
+                break;
+            case "end":
+                $logType = "NOTICE";
+                $logMessage = "Backup finished successfully";
+                break;
+            case "error":
+                $logType = "ERROR";
+                if (!is_null($message)) {
+                    $logMessage = $message . "...Script aborted";
+                } else {
+                    $logMessage = "An error has occurred";
+                }
+                break;
+        }
+
+        $entry = sprintf("%s %s %s %s\r\n", date("Y-m-d H:i:s"), "-- PHPBackupScript -", $logType, $logMessage);
+        file_put_contents($path, $entry, FILE_APPEND);
+
+        if ($type == "error") {
+            exit;
+        }
+
+    }
+
+
+    //getter and setter
+
+    /**
+     * @return null|string
+     */
+    public function getMail()
+    {
+        return $this->_mail;
+    }
+
+    /**
+     * @param null|string $email
+     */
+    public function setMail($email)
+    {
+        $this->_mail = $email;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getCreateLog()
+    {
+        return $this->_createLog;
+    }
+
+    /**
+     * @param boolean $createLog
+     */
+    public function setCreateLog($createLog)
+    {
+        $this->_createLog = $createLog;
     }
 
 }
